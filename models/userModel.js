dd9f76d79d8f908aabd621891c4e55a477efa892
techscaleups <info.techscaleup@gmail.com>
2025-07-06 22:58:25 +0530

    Initial backend commit

--- models/userModel.js
+++ models/userModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email:    { type: String, required: true, unique: true },
-  number:   { type: String, required: true },
-  profile:  { type: String }, // base64 image string
+  email: { type: String, required: true, unique: true },
+  number: { type: String, required: true },
+  profile: { type: String }, // can be image URL or base64
   password: { type: String, required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
