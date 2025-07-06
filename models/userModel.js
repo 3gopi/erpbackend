1ef9e77762b9dd391b42768f0bea7e0586c903e6
techscaleups <info.techscaleup@gmail.com>
2025-07-06 23:08:10 +0530

    Initial backend commit

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   number: { type: String, required: true },
-  profile: { type: String }, // can be image URL or base64
+  profile: { type: String },
   password: { type: String, required: true }
 }, { timestamps: true });
 
