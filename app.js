2647d777d7a58bff224b2a3e52f9184f303c1beb
techscaleups <info.techscaleup@gmail.com>
2025-07-06 17:01:45 +0530

    Initial backend commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('ERP Backend is working!');
+  res.send('ERP Backend is Api!');
 });
 
 app.use('/api/auth', authRoutes);
