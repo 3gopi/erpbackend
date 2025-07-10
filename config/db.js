31afb97079f99643f04f3b31f29a909ca4f5d698
techscaleups <info.techscaleup@gmail.com>
2025-07-10 08:27:48 +0530

    Trigger ERP deploy

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/erp_db', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect('mongodb://127.0.0.1:27017/erp_db');
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection error:', err.message);
